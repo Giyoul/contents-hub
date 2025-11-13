e840939ae7298860a2476174d49bf36466823abf
Giyoul <cla12093@gmail.com>
2025-11-13 17:15:00 +0900

    feat: add getting post information by post id feature ;

--- src/service/postService.ts
+++ src/service/postService.ts
@@ -13,3 +13,18 @@ export async function getPostByCategoryId(categoryId: string): Promise<any> {
 
 	return data || [];
 }
+
+export async function getPostByID(postId: string): Promise<any> {
+	const { data, error } = await supabase
+		.from('post')
+		.select('*, discussion (*), resource (*)')
+		.eq('id', postId)
+		.single()
+
+	if (error) {
+		console.error(error)
+		return [];
+	}
+
+	return data || [];
+}
